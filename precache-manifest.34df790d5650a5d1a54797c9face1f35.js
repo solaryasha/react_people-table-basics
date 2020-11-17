f8bae38f98506f92d8e1d7a928d9f0086097e336
Volodymyr Pavlenko <volodymyrpavlenko@Volodymyrs-MBP.mate.academy>
2020-11-17 11:14:47 +0200

    Updates

---
+++ precache-manifest.34df790d5650a5d1a54797c9face1f35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ed88acef20a659f3afa0fd7f1f9019",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b4ad792b388f3b44ebf1",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "bc1b4e2409c19d6dbe4c",
+    "url": "./static/js/2.4b9414c3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.4b9414c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ad792b388f3b44ebf1",
+    "url": "./static/js/main.5f676268.chunk.js"
+  },
+  {
+    "revision": "c7ae2510e9f54903a2ed",
+    "url": "./static/js/runtime-main.b51b247a.js"
+  }
+]);
